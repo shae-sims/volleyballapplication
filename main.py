ed1d03a427ffa7dd73b16ce53e7941761bbd85f1
ShaeSims <seldredge20@gmail.com>
2024-11-28 13:19:43 -0700

    add explanation to the second tab

--- main.py
+++ main.py
@@ -173,3 +173,29 @@ elif st.session_state.current_page == "Graphics and Exploration":
             fig5 = barplot(data, 'Name', var, number, direct)
             st.plotly_chart(fig5)
 
+        with st.expander("Explanation of Skills"):
+        st.write('''
+        Rank: The ranking of the players from 1-168. One being high.
+                    
+        Games Played: The number of games an athlete competed in (also known as matchs). One game is composed of 3-5 different sets.
+                    
+        Sets Played: The number os sets played by the athletes. A set goes to 25 and you must win by 2. The fifth set goes to 15 if necessary. 
+                    You must win 3 sets to win the game.
+
+        Kills/ Kills per Set: A kill is when a player hits the ball and it results in a point for their team. 
+                    Kills is the total number and kills per set is based of each individual set.
+
+        Hitting Percentage: A percentage ranging from 1 to -1. Players are ranked off of kills, hitting errors, and hitting attempts (player hits the ball and it is played by the other team)
+                    If they have all kills it will be a 1, if they have all errors it is a -1.
+
+        Assists/ Assists per Set: An offensive move that gives another player an opportunity to hit the ball.
+                    
+        Digs/ Digs per Set: When a player successfully passes a hard driven ball. 
+                    
+        Service Aces/ Aces per Set: When a player serves the ball and recieves a point.
+                    
+        Reception Percentage: The percentage that a player has a good pass when receiving a serve.
+        
+        ''')
+
+
