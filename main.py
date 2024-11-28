3f214bb1deec49a049323cfe0ae9e28277f376d6
ShaeSims <seldredge20@gmail.com>
2024-11-27 22:21:40 -0700

    add data and title 1st plot

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import zipfile
+import plotly.express as px
+import matplotlib.pyplot as plt
+import requests
+from io import BytesIO
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from plots import *
+import streamlit as st
+
+data = pd.read_csv("data2023Clean.csv")
+
+st.title('Big 10 Volleyball Ranking Comparions 2023')
+
+fig1 = rank_comparison(data, y = "Blocks per Set")
---
+++ plots.py
@@ -0,0 +1,46 @@
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def rank_comparison(data, x = "Rank", y = "Kills"):
+    if y == 'Blocks per Set':
+        z = 'Blocks_Per_Set'
+    
+    elif y == 'Games Played':
+        z = 'Games_Played'
+
+    elif y == 'Sets Played':
+        z = 'Sets_Played'
+    
+    elif y == 'Kills per Set':
+        z = 'Kills_Per_Set'
+    
+    elif y == 'Hitting Percentage':
+       z = 'Hitting_Percentage'
+    
+    elif y == 'Assists per Set':
+        z ='Assists_Per_set'
+    
+    elif y == 'Blocks per Set':
+        z = 'Blocks_Per_Set'
+
+    elif y == 'Digs per Set':
+        z = 'Digs_Per_Set'
+    
+    elif y == 'Service Aces':
+        z = 'Service_Aces'
+
+    elif y == 'Aces per Set':
+        z = 'Service_Aces_Per_Set'
+
+    elif y == 'Reception Percentage':
+        z ='Reception_Percentage'
+    
+    else:
+        z = y
+        
+    fig = px.scatter(data, x, z, title=f"Rank compared to {y}")
+    return fig
